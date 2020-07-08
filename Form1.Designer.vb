ae55db1eb4f632cc54dcdd44872f0c7f0a7d08a9
leo <tohatsu4s@hotmail.fr>
2020-07-08 13:34:37 +0200

    Upload du projet

---
+++ Form1.Designer.vb
@@ -0,0 +1,225 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FormPrincipale
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.txtSaisie = New System.Windows.Forms.TextBox()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.gboProposition = New System.Windows.Forms.GroupBox()
+        Me.btnTester = New System.Windows.Forms.Button()
+        Me.gboEssais = New System.Windows.Forms.GroupBox()
+        Me.lstPropo = New System.Windows.Forms.ListBox()
+        Me.lblTitrePropo = New System.Windows.Forms.Label()
+        Me.lblNbRestant = New System.Windows.Forms.Label()
+        Me.lblRestants = New System.Windows.Forms.Label()
+        Me.lblHisto = New System.Windows.Forms.Label()
+        Me.lstHisto = New System.Windows.Forms.ListBox()
+        Me.lblTitreMot = New System.Windows.Forms.Label()
+        Me.lblMot = New System.Windows.Forms.Label()
+        Me.btnRestart = New System.Windows.Forms.Button()
+        Me.btnQuitter = New System.Windows.Forms.Button()
+        Me.gboProposition.SuspendLayout()
+        Me.gboEssais.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtSaisie
+        '
+        Me.txtSaisie.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.txtSaisie.Location = New System.Drawing.Point(15, 19)
+        Me.txtSaisie.Name = "txtSaisie"
+        Me.txtSaisie.ShortcutsEnabled = False
+        Me.txtSaisie.Size = New System.Drawing.Size(129, 20)
+        Me.txtSaisie.TabIndex = 0
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'gboProposition
+        '
+        Me.gboProposition.Controls.Add(Me.btnTester)
+        Me.gboProposition.Controls.Add(Me.txtSaisie)
+        Me.gboProposition.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.gboProposition.Location = New System.Drawing.Point(75, 99)
+        Me.gboProposition.Name = "gboProposition"
+        Me.gboProposition.Size = New System.Drawing.Size(233, 53)
+        Me.gboProposition.TabIndex = 2
+        Me.gboProposition.TabStop = False
+        Me.gboProposition.Text = "Proposition"
+        '
+        'btnTester
+        '
+        Me.btnTester.Enabled = False
+        Me.btnTester.Location = New System.Drawing.Point(150, 16)
+        Me.btnTester.Name = "btnTester"
+        Me.btnTester.Size = New System.Drawing.Size(75, 23)
+        Me.btnTester.TabIndex = 3
+        Me.btnTester.Text = "Tester"
+        Me.btnTester.UseVisualStyleBackColor = True
+        '
+        'gboEssais
+        '
+        Me.gboEssais.Controls.Add(Me.lstPropo)
+        Me.gboEssais.Controls.Add(Me.lblTitrePropo)
+        Me.gboEssais.Controls.Add(Me.lblNbRestant)
+        Me.gboEssais.Controls.Add(Me.lblRestants)
+        Me.gboEssais.Location = New System.Drawing.Point(75, 203)
+        Me.gboEssais.Name = "gboEssais"
+        Me.gboEssais.Size = New System.Drawing.Size(233, 185)
+        Me.gboEssais.TabIndex = 3
+        Me.gboEssais.TabStop = False
+        Me.gboEssais.Text = "Essais"
+        '
+        'lstPropo
+        '
+        Me.lstPropo.FormattingEnabled = True
+        Me.lstPropo.Location = New System.Drawing.Point(6, 70)
+        Me.lstPropo.Name = "lstPropo"
+        Me.lstPropo.Size = New System.Drawing.Size(219, 108)
+        Me.lstPropo.TabIndex = 4
+        '
+        'lblTitrePropo
+        '
+        Me.lblTitrePropo.AutoSize = True
+        Me.lblTitrePropo.Location = New System.Drawing.Point(6, 54)
+        Me.lblTitrePropo.Name = "lblTitrePropo"
+        Me.lblTitrePropo.Size = New System.Drawing.Size(70, 13)
+        Me.lblTitrePropo.TabIndex = 4
+        Me.lblTitrePropo.Text = "Propositions :"
+        '
+        'lblNbRestant
+        '
+        Me.lblNbRestant.AutoSize = True
+        Me.lblNbRestant.Location = New System.Drawing.Point(67, 29)
+        Me.lblNbRestant.Name = "lblNbRestant"
+        Me.lblNbRestant.Size = New System.Drawing.Size(19, 13)
+        Me.lblNbRestant.TabIndex = 4
+        Me.lblNbRestant.Text = "nb"
+        '
+        'lblRestants
+        '
+        Me.lblRestants.AutoSize = True
+        Me.lblRestants.Location = New System.Drawing.Point(6, 29)
+        Me.lblRestants.Name = "lblRestants"
+        Me.lblRestants.Size = New System.Drawing.Size(55, 13)
+        Me.lblRestants.TabIndex = 4
+        Me.lblRestants.Text = "Restants :"
+        '
+        'lblHisto
+        '
+        Me.lblHisto.AutoSize = True
+        Me.lblHisto.Location = New System.Drawing.Point(553, 86)
+        Me.lblHisto.Name = "lblHisto"
+        Me.lblHisto.Size = New System.Drawing.Size(74, 13)
+        Me.lblHisto.TabIndex = 4
+        Me.lblHisto.Text = "HISTORIQUE"
+        '
+        'lstHisto
+        '
+        Me.lstHisto.FormattingEnabled = True
+        Me.lstHisto.Location = New System.Drawing.Point(506, 115)
+        Me.lstHisto.Name = "lstHisto"
+        Me.lstHisto.Size = New System.Drawing.Size(175, 186)
+        Me.lstHisto.TabIndex = 5
+        '
+        'lblTitreMot
+        '
+        Me.lblTitreMot.AutoSize = True
+        Me.lblTitreMot.Location = New System.Drawing.Point(142, 20)
+        Me.lblTitreMot.Name = "lblTitreMot"
+        Me.lblTitreMot.Size = New System.Drawing.Size(85, 13)
+        Me.lblTitreMot.TabIndex = 6
+        Me.lblTitreMot.Text = "Mot à ordonner :"
+        '
+        'lblMot
+        '
+        Me.lblMot.AutoSize = True
+        Me.lblMot.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMot.Location = New System.Drawing.Point(157, 54)
+        Me.lblMot.Name = "lblMot"
+        Me.lblMot.Size = New System.Drawing.Size(51, 20)
+        Me.lblMot.TabIndex = 7
+        Me.lblMot.Text = "lblMot"
+        '
+        'btnRestart
+        '
+        Me.btnRestart.Location = New System.Drawing.Point(506, 358)
+        Me.btnRestart.Name = "btnRestart"
+        Me.btnRestart.Size = New System.Drawing.Size(75, 23)
+        Me.btnRestart.TabIndex = 4
+        Me.btnRestart.Text = "Rejouer"
+        Me.btnRestart.UseVisualStyleBackColor = True
+        '
+        'btnQuitter
+        '
+        Me.btnQuitter.Location = New System.Drawing.Point(606, 358)
+        Me.btnQuitter.Name = "btnQuitter"
+        Me.btnQuitter.Size = New System.Drawing.Size(75, 23)
+        Me.btnQuitter.TabIndex = 8
+        Me.btnQuitter.Text = "Quitter"
+        Me.btnQuitter.UseVisualStyleBackColor = True
+        '
+        'FormPrincipale
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnQuitter)
+        Me.Controls.Add(Me.btnRestart)
+        Me.Controls.Add(Me.lblMot)
+        Me.Controls.Add(Me.lblTitreMot)
+        Me.Controls.Add(Me.lstHisto)
+        Me.Controls.Add(Me.lblHisto)
+        Me.Controls.Add(Me.gboEssais)
+        Me.Controls.Add(Me.gboProposition)
+        Me.Name = "FormPrincipale"
+        Me.Text = "Jeu de mots"
+        Me.gboProposition.ResumeLayout(False)
+        Me.gboProposition.PerformLayout()
+        Me.gboEssais.ResumeLayout(False)
+        Me.gboEssais.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtSaisie As TextBox
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents gboProposition As GroupBox
+    Friend WithEvents btnTester As Button
+    Friend WithEvents gboEssais As GroupBox
+    Friend WithEvents lblRestants As Label
+    Friend WithEvents lstPropo As ListBox
+    Friend WithEvents lblTitrePropo As Label
+    Friend WithEvents lblNbRestant As Label
+    Friend WithEvents lblHisto As Label
+    Friend WithEvents lstHisto As ListBox
+    Friend WithEvents lblTitreMot As Label
+    Friend WithEvents lblMot As Label
+    Friend WithEvents btnRestart As Button
+    Friend WithEvents btnQuitter As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,137 @@
+﻿Public Class FormPrincipale
+
+    Private tabMots() As String 'Tableau contenant les mots possibles à deviner
+    Private Const CONST_ESSAIS As Integer = 3 'Le nombre maximal d'essais auquel à droit l'utilisateur (appelé pour le reinitialiser le nombre d'essais restants lors d'une nouvelle partie)
+    Private essais As Integer = 3 'Le Le nombre d'essais restants (mit a jour au fur et à mesure des tentatives et des parties)
+    Private motAtrouver As String 'String contenant le mot à trouver (récupéré dans le tableau)
+    Private random As New Random() 'Initialise l'objet permettant de déterminer les entiers aléatoires
+    Private cptVictoire As Integer 'Compteur de victoire (pour le résumé lors de la fermeture de l'application)
+    Private cptDefaite As Integer 'Compteur de defaites (pour le résumé lors de la fermeture de l'application)
+    Private nbParties As Integer = 0 'Compteur du nombre de parties jouées (Utilisé pour numéroter les parties dans l'historique
+    Private nbCoups As Integer = 0 'Compteur du nombre de coups tentés par l'utilisateur pour la partie en cours (utile lors de la victoire)
+
+    ''' <summary>
+    ''' Procédure permettant d'afficher la message box donnant le résumé des parties jouées (nombre victoires/defaites)
+    ''' Appelée dans tout les cas ou l'application ferme
+    ''' </summary>
+    Private Sub msgBox_resultat()
+        MessageBox.Show("Nombre de defaites : " & cptDefaite & vbCrLf & "Nombre de victoires : " & cptVictoire, "Fermeture")
+    End Sub
+
+    ''' <summary>
+    ''' Procédure gérant la victoire. Affiche une boite de dialogue annoncant la victoire et invitant l'utilisateur à recommencer.
+    ''' </summary>
+    Private Sub motCorrect()
+        If MessageBox.Show("Voulez vous rejouer ?", "VICTOIRE", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
+            'Si oui
+            lstHisto.Items.Add("PARTIE " & nbParties & " : VICTOIRE (" & nbCoups & " coup(s))") 'La partie est ajoutée à l'historique des parties en précisant la victoire et le nombre de coups ayant été nécéssaires
+            cptVictoire = cptVictoire + 1 'Le nombre de victoires est incrémenté de 1 pour le résumé lors de la fermeture de l'application
+            nouvellePartie() 'Appelle la procédure permettant de lancer une nouvelle partie
+        Else msgBox_resultat() 'Si non, affiche le résumé des parties jouées (nombre victoires/defaites)
+            Me.Dispose() 'Puis l'application se ferme
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Procédure gérant la défaite. Affiche une boite de dialogue affichant le mot qui était à deviner et invitant l'utilisateur à recommencer.
+    ''' </summary>
+    Private Sub motIncorrect()
+        If MessageBox.Show("Le mot à deviner était " & motAtrouver & vbCrLf & "Voulez vous rejouer ?", "DEFAITE", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
+            'Si oui
+            lstHisto.Items.Add("PARTIE " & nbParties & " : DEFAITE") 'La partie est ajoutée à l'historique des parties en précisant la defaite
+            cptDefaite = cptDefaite + 1 'Le nombre de défaites est incrémenté de 1 pour le résumé lors de la fermeture de l'application
+            nouvellePartie() 'Appelle la procédure permettant de lancer une nouvelle partie
+        Else msgBox_resultat() 'Si non, affiche le résumé des parties jouées (nombre victoires/defaites)
+            Me.Dispose() 'Puis l'application se ferme
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Fonction qui renvoie une chaine de caractere resultant du mélange aléatoire des lettres de la chaine de caractère passée en parametre (mot du tableau).
+    ''' </summary>
+    ''' <param name="chaine">chaine de caractère à mélanger</param>
+    ''' <returns>chaine de caractère mélangée</returns>
+    Private Function melanger(ByVal chaine As String) As String
+        Dim pos As Integer 'Position (aléatoire) ou sera placée chaque lettre dans la nouvelle chaine qui sera renvoyée
+        Dim mixed As String = "" 'Chaine qui sera renvoyée
+        For Each lettre In chaine 'Pour chaque lettre de la chaine en parametre
+            pos = random.Next(0, mixed.Length + 1) 'Désigner une position au hasard
+            mixed = mixed.Insert(pos, lettre) 'Placer la lettre à la position déterminé précédemment (dans la chaine de caractere à renvoyer)
+        Next
+        Return mixed
+    End Function
+
+    ''' <summary>
+    ''' Fonction permettant essentiellement de mettre à jour et reinitialiser les variables nécéssaires au passage d'une partie à la suivante.
+    ''' </summary>
+    Private Sub nouvellePartie()
+        nbCoups = 0 'Le nombre de coups tentés par l'utilisateur est remit à 0.
+        nbParties = nbParties + 1 'Compteur de parties pour donner le numero des parties dans l'historique.
+        essais = CONST_ESSAIS 'Le nombre d'essais restants pour l'utilisateur.
+        lblNbRestant.Text = essais 'Affiche le nombre d'essais restants à l'utilisateur (initialisé juste au-dessus).
+        lstPropo.Items.Clear() 'Remise à zéro de la liste des propositions de l'utilisateur émisent lors de la partie précédente.
+        Dim rndnbr As Integer
+        rndnbr = random.Next(0, tabMots.Length) 'Désigne au hasard un nouveau mot à trouver dans le tableau (par son indice)
+        motAtrouver = tabMots(rndnbr) 'Récupération du mot dans une variable String
+        lblMot.Text = melanger(motAtrouver) 'Affichage du mot mélangé pour l'utilisateur
+    End Sub
+
+    ''' <summary>
+    ''' Procédure réalisant les initialisations nécéssaires au démarrage du jeu (tableau de mot) et appelant la procédure permettant de lancer une nouvelle partie.
+    ''' </summary>
+    Private Sub initialisation()
+        tabMots = {"BONJOUR", "CASQUE", "VOITURE"}
+        nouvellePartie()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load 'initialisation est appelée lors du premier chargement de l'application
+        initialisation()
+    End Sub
+
+    ''' <summary>
+    ''' Demande à l'utilisateur s'il veut réellement quitter le jeu quand il clique sur le bouton quitter. S'il repond oui, le jeu lui résume son nombre de victoires et defaites avant de fermer.
+    ''' </summary>
+    Private Sub BtnQuitter_Click(sender As Object, e As EventArgs) Handles btnQuitter.Click
+        If MessageBox.Show("Voulez vous vraiment quitter?", "Confirmation exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
+            msgBox_resultat()
+            Me.Dispose()
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Demande à l'utilisateur s'il veut annuler la manche en cours et en recommencer une. S'il repond oui, la fonction nouvellePartie est appelée.
+    ''' </summary>
+    Private Sub BtnRestart_Click(sender As Object, e As EventArgs) Handles btnRestart.Click
+        If MessageBox.Show("Annuler la partie en cours ?", "Recommencer une partie", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
+            nouvellePartie()
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Procédure permettant à l'utilisateur de soumettre sa proposition.
+    ''' </summary>
+    Private Sub BtnTester_Click(sender As Object, e As EventArgs) Handles btnTester.Click
+        lstPropo.Items.Add(txtSaisie.Text) 'Rajoute la tentative dans la liste des tentatives
+        nbCoups = nbCoups + 1 'Dans tout les cas, le nombre de tentative de l'utilisateur pour la manche en cours est incrémenté de 1.
+        If txtSaisie.Text = motAtrouver Then 'Si l'utilisateur devine le mot
+            motCorrect() 'Appel la procédure gérant la victoire
+        Else 'Sinon
+            essais = essais - 1 'Décrémente de 1 son nombre d'essais restants
+            lblNbRestant.Text = essais 'Met a jour l'affichage lui indiquant son nombre d'essais restants
+        End If
+        txtSaisie.Text = "" 'La zone de texte est remise à zero
+        If essais = 0 Then 'Si le nombre d'essais restants de l'utilisateur atteint 0, la procédure gérant la défaite est appelée.
+            motIncorrect()
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Procédure empechant l'utilisateur de tester sa proposition si cette derniere est nulle (désactive le bouton tester).
+    ''' </summary>
+    Private Sub txtSaisie_TextChanged(sender As Object, e As EventArgs) Handles txtSaisie.TextChanged
+        If txtSaisie.Text.Length > 0 Then
+            btnTester.Enabled = True
+        Else btnTester.Enabled = False
+        End If
+    End Sub
+End Class
